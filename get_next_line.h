4cc53d9defdbf97057399e4634ced6c8a955624c
Kimy Lucchini <lucchini.kimy@gmail.com>
2025-11-25 20:56:59 +0100

    Created all necessary files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klucchin <klucchin@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 20:50:29 by klucchin          #+#    #+#             */
+/*   Updated: 2025/11/25 20:53:29 by klucchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klucchin <klucchin@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 20:55:58 by klucchin          #+#    #+#             */
+/*   Updated: 2025/11/25 20:56:09 by klucchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line_bonus(int fd);
+
+#endif
